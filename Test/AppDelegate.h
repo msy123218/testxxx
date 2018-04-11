c598211c50414626c94e658930a9e70a54ed01b3
yuese521 <liyi@yktour.com.cn>
2018-04-11 10:34:14 +0800

    xxx

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by admin on 2018/4/11.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
